ac7f8f97fca94888cb8bc75434899033f3de0f92
Yoctopuce <dev@yoctopuce.com>
2018-09-19 15:24:35 +0200

    "New build based on revison 32188"

--- API/yocto_api.cs
+++ API/yocto_api.cs
@@ -1218,7 +1218,7 @@ public class YAPI
     public const string YOCTO_API_VERSION_STR = "1.10";
     public const int YOCTO_API_VERSION_BCD = 0x0110;
 
-    public const string YOCTO_API_BUILD_NO = "32155";
+    public const string YOCTO_API_BUILD_NO = "32188";
     public const int YOCTO_DEFAULT_PORT = 4444;
     public const int YOCTO_VENDORID = 0x24e0;
     public const int YOCTO_DEVID_FACTORYBOOT = 1;
--- constants.cs
+++ constants.cs
@@ -55,7 +55,7 @@ namespace YoctoVisualisation
   class constants
   {
 
-    public static string buildVersion = "32155";
+    public static string buildVersion = "32188";
     private static string _configfile = Path.Combine(Application.UserAppDataPath, "config.xml");
     private static bool _configfileOveridden = false;
     public static int MAXRAWDATAROWS = 2000;
